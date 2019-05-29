107e03c7bea5c545ac99e0e8584664795b587965
mansi <nachankarmansi@gmail.com>
2019-05-29 21:42:19 +0530

    add-product added

---
+++ product.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/product',(request,response)=>
+{
+   
+console.log('post created');
+
+});
+
+
+
+module.exports=router;
